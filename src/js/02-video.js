432b6fe1ef717d31f6039708a2b7024355b292b7
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2023-08-21 18:50:03 +0200

    update of JS HW 08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,16 @@ console.log(Player);
 const vimeoPlayer = new Player('vimeo-player');
 const timeStorage = 'videoplayer-current-time';
 
+let playDuration = 0;
+
+Player.getDuration().then(duration => {
+  playDuration = duration;
+});
+
 vimeoPlayer.on('timeupdate', throttle(updateTime, 1000));
+vimeoPlayer.on('ended', () => {
+  localStorage.removeItem(timeStorage);
+});
 
 function updateTime(e) {
   const currentTime = e.seconds;
@@ -13,10 +22,14 @@ function updateTime(e) {
 
 function loadVideo() {
   const savedTime = localStorage.getItem(timeStorage);
-
   if (savedTime) {
     vimeoPlayer.setCurrentTime(parseFloat(savedTime));
   }
 }
-
+function onVideoEnd() {
+  if (savedTime < playDuration) {
+    vimeoPlayer.setCurrentTime(savedTime);
+  }
+}
 loadVideo();
+onVideoEnd();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,7 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('[name="email"]');
 const messageInput = form.querySelector('[name="message"]');
+const submitButton = form.querySelector('button[type="submit"]');
 const formStorageKey = 'feedback-form-state';
 
 form.addEventListener('input', throttle(saveForm, 500));
@@ -43,3 +44,15 @@ function handleSubmit(e) {
   emailInput.value = '';
   messageInput.value = '';
 }
+form.addEventListener('input', () => {
+  saveForm();
+  disableSubmit();
+});
+function disableSubmit() {
+  if (emailInput.value.length === 0 || messageInput.value.length === 0) {
+    submitButton.setAttribute('disabled', 'true');
+  } else {
+    submitButton.removeAttribute('disabled', 'true');
+  }
+}
+disableSubmit();
